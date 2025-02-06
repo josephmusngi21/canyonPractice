f101a0c2f1ad9fe025118f885c982806ed9e71cf
Josephmusngi21 <josephmusngi7@gmail.com>
2025-02-06 00:05:46 -0700

    started timer

--- client/components/Timer/Timer.jsx
+++ client/components/Timer/Timer.jsx
@@ -1,10 +1,42 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, Button } from "react-native";
 
 export default function Timer() {
-  return (
-    <View>
-      <Text>Timer</Text>
-    </View>
-  );
+    const [time, setTime] = useState(0.00);
+    const [pressed, setPressed] = useState(false);
+
+    useEffect(() => {
+        let interval;
+        if (pressed) {
+            interval = setInterval(() => {
+                setTime(prevTime => prevTime + 0.01);
+            }, 10);
+        } else if (!pressed && time !== 0) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [pressed]);
+
+    const onStart = () => {
+        setPressed(true);
+        console.log('Button clicked');
+    };
+
+    const onEnd = () => {
+        setPressed(false);
+        console.log('Button clicked');
+    };
+
+    return (
+        <View>
+            <Text>{time.toFixed(2)}</Text>
+            {!pressed && <Button onPress={onStart} title="Start" />}
+            {pressed && (
+                <>
+                    <Button onPress={onEnd} title="End" />
+                    <Button onPress={onStart} title="Start" />
+                </>
+            )}
+        </View>
+    );
 }
