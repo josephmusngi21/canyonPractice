37a6fc0eb19b022fd91f8ac060be81b57618ea03
Josephmusngi21 <josephmusngi7@gmail.com>
2025-02-09 11:32:21 -0700

    imported tracking

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text, View } from "react-native";
+import Tracking from '../components/geolocation/tracking/Tracking.jsx';
 
 export default function Index() {
   return (
