4607c0d46f8a3b4cd2fb7a55c26f4cdc9677710a
Josephmusngi21 <josephmusngi7@gmail.com>
2025-02-09 11:57:01 -0700

    updated to have tracking

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import Tracking from '../components/geolocation/tracking/Tracking.jsx';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Tracking />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
